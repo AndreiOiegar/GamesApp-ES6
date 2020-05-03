308f93a4a69bbacf876662abf9bdd8fc4fd28cb8
AndreiOiegar <andrei.oiegar@gmail.com>
2020-05-03 18:07:28 +0300

    FetchApi converted to ES6

--- Classes/fetchApi.js
+++ Classes/fetchApi.js
@@ -1,39 +1,87 @@
-function FetchApi(apiURL){
-    this.apiURL = apiURL;
-}
+// function FetchApi(apiURL){
+//     this.apiURL = apiURL;
+// }
 
-FetchApi.prototype.getGamesList = function(){
+class FetchApi {
+    constructor(apiURL){
+        this.apiURL = apiURL;
+    }
+
+
+// FetchApi.prototype.getGamesList = function(){
+//     return fetch(`${this.apiURL}` + "/games", {
+//         method: "GET",
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded"
+//         } 
+//     })
+//     .then(response => response.json());
+// }
+
+getGamesList(){
     return fetch(`${this.apiURL}` + "/games", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        } 
-    })
-    .then(response => response.json());
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                } 
+            })
+            .then(response => response.json());
 }
 
-FetchApi.prototype.deleteGame = function(gameID){
+// FetchApi.prototype.deleteGame = function(gameID){
+//     return fetch(`${this.apiURL}` + "/games/" + gameID, {
+//         method: "DELETE",
+//     }).then(r => r.text());
+// }
+
+
+deleteGame(gameID){
     return fetch(`${this.apiURL}` + "/games/" + gameID, {
         method: "DELETE",
     }).then(r => r.text());
+    
 }
 
-FetchApi.prototype.createGameRequest = function(){
+
+// FetchApi.prototype.createGameRequest = function(){
+//     return fetch(`${this.apiURL}` + "/games/", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded"
+//         },
+//         body: gameObject
+//     }).then(response => response.json());
+// }
+
+
+createGameRequest(){
     return fetch(`${this.apiURL}` + "/games/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: gameObject
-    }).then(response => response.json());
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: gameObject
+            }).then(response => response.json());
 }
 
-FetchApi.prototype.updateGameRequest = function(gameId, updatedGameObj){
+// FetchApi.prototype.updateGameRequest = function(gameId, updatedGameObj){
+//     return fetch(`${this.apiURL}` + "/games/" + gameId, {
+//         method: "PUT",
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded"
+//         },
+//         body: updatedGameObj
+//     }).then(response => response.json())
+// }
+
+updateGameRequest(){
     return fetch(`${this.apiURL}` + "/games/" + gameId, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: updatedGameObj
-    }).then(response => response.json())
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: updatedGameObj
+            }).then(response => response.json())
+}
+
 }
